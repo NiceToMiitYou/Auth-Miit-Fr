ddfa9f8a4aa70cda2d1927ec4ce8b55b584ba143
Boris Tacyniak <boris.tacyniak@itevents.fr>
2014-12-11 20:59:38 +0100

    Add qualification configuration

---
+++ config/env/qualification.js
@@ -0,0 +1,31 @@
+/**
+ * Qualification environment settings
+ *
+ * This file can include shared settings for a development team,
+ * such as API keys or remote database passwords.  If you're using
+ * a version control solution for your Sails app, this file will
+ * be committed to your repository unless you add it to your .gitignore
+ * file.  If your repository will be publicly viewable, don't add
+ * any private information to this file!
+ *
+ */
+
+module.exports = {
+
+  /***************************************************************************
+   * Set the default database connection for models in the development       *
+   * environment (see config/connections.js and config/models.js )           *
+   ***************************************************************************/
+
+  connections: {
+
+    DwhWebService: {
+        args: {
+            accessToken: 'qVZDWicwFjh49O9PQUKJ8Ur8r3YKSrx3YqUll2L6'
+        },
+        protocol: 'http',
+        host: 'ws.qlf.priv.miit.fr',
+        port: 80
+    }
+  }
+};
