5778133564afb0d8710854a1d03abc7ebccfa733
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-01-18 22:20:10 +0100

    [i18n] Change to object notation

--- config/i18n.js
+++ config/i18n.js
@@ -17,41 +17,44 @@
 
 module.exports.i18n = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Which locales are supported?                                             *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+    *                                                                          *
+    * Which locales are supported?                                             *
+    *                                                                          *
+    ***************************************************************************/
 
-  locales: ['fr'],
+    locales: ['fr'],
 
-  /****************************************************************************
-  *                                                                           *
-  * What is the default locale for the site? Note that this setting will be   *
-  * overridden for any request that sends an "Accept-Language" header (i.e.   *
-  * most browsers), but it's still useful if you need to localize the         *
-  * response for requests made by non-browser clients (e.g. cURL).            *
-  *                                                                           *
-  ****************************************************************************/
+    /****************************************************************************
+    *                                                                           *
+    * What is the default locale for the site? Note that this setting will be   *
+    * overridden for any request that sends an "Accept-Language" header (i.e.   *
+    * most browsers), but it's still useful if you need to localize the         *
+    * response for requests made by non-browser clients (e.g. cURL).            *
+    *                                                                           *
+    ****************************************************************************/
 
-  defaultLocale: 'fr',
+    defaultLocale: 'fr',
 
-  /****************************************************************************
-  *                                                                           *
-  * Automatically add new keys to locale (translation) files when they are    *
-  * encountered during a request?                                             *
-  *                                                                           *
-  ****************************************************************************/
 
-  // updateFiles: false,
+    objectNotation: true
 
-  /****************************************************************************
-  *                                                                           *
-  * Path (relative to app root) of directory to store locale (translation)    *
-  * files in.                                                                 *
-  *                                                                           *
-  ****************************************************************************/
+    /****************************************************************************
+    *                                                                           *
+    * Automatically add new keys to locale (translation) files when they are    *
+    * encountered during a request?                                             *
+    *                                                                           *
+    ****************************************************************************/
 
-  // localesDirectory: '/config/locales'
+    // updateFiles: false,
+
+    /****************************************************************************
+    *                                                                           *
+    * Path (relative to app root) of directory to store locale (translation)    *
+    * files in.                                                                 *
+    *                                                                           *
+    ****************************************************************************/
+
+    // localesDirectory: '/config/locales'
 
 };
