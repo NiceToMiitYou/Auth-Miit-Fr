deed850550bee9bce6540c3a6a954cadd5051b74
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-03-16 18:57:27 +0100

    [ENHANCEMENT] Add connection bar in the project (if socket disconnect)

---
+++ assets/js/controllers/connection-bar.js
@@ -0,0 +1,96 @@
+"use strict";
+
+
+angular
+    .module( 'MiitAuth')
+    .controller( 'ConnectionBarController', [
+        '$scope', '$timeout',
+        function( $scope, $timeout ) {
+        
+            $scope.isDisplay     = false;
+            $scope.isAlert       = true;
+
+            function hide( cb ) {
+
+                $timeout( function() {
+                    $scope.isAlert       = false;
+
+                    $timeout( function() {
+                        $scope.isDisplay     = false;
+
+                        if( typeof cb === 'function' ) {
+
+                            cb();
+                        }
+                    }, 3500 );
+                } );
+            }
+
+            function show( ) {
+
+                $timeout( function() {
+
+                    $scope.isAlert   = true;
+
+                    $scope.isDisplay = true;
+                } );
+            }
+
+            // Fired upon a successful connection.
+            function onConnect() {
+                
+            }
+
+            // Fired upon a connection error.
+            function onConnectError( error ) {
+
+                show();
+            }
+
+            // Fired upon a connection timeout.
+            function onConnectTimeout() {
+
+                show();
+            }
+
+            // Fired upon a successful reconnection.
+            function onReconnect( attempt ) {
+
+                hide();
+            }
+            
+            // Fired upon an attempt to reconnect.
+            function onReconnectAttempt() {
+
+                show();
+            }
+
+            // Fired upon an attempt to reconnect.
+            function onReconnecting( attempt ) {
+
+                show();
+            }
+
+            // Fired upon a reconnection attempt error.
+            function onReconnectError( error ) {
+
+                show();
+            }
+
+            // Fired when couldn’t reconnect within reconnectionAttempts
+            function onReconnectFailed( attempt ) {
+
+                show();
+            }
+
+            // Bind all events
+            MiitConnect.listen( 'connect',           onConnect );
+            MiitConnect.listen( 'connect_error',     onConnectError );
+            MiitConnect.listen( 'connect_timeout',   onConnect );
+            MiitConnect.listen( 'reconnect',         onReconnect );
+            MiitConnect.listen( 'reconnect_attempt', onReconnectAttempt );
+            MiitConnect.listen( 'reconnecting',      onReconnecting); 
+            MiitConnect.listen( 'reconnect_error',   onReconnectError); 
+            MiitConnect.listen( 'reconnect_failed',  onReconnectFailed); 
+        }
+    ] );
