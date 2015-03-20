f70b10cc4daa74923fb1b1ced9b2210e0cd64386
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-03-20 18:16:01 +0100

    Revert "[ENHANCEMENT] Record http logs"
    This reverts commit 0031ed9742a0a3a2e90d54958a7c45995c49f038.

--- config/env/production.js
+++ config/env/production.js
@@ -10,8 +10,7 @@
  *
  */
 
-var fs              = require('fs'),
-    accessLogStream = fs.createWriteStream( __dirname + '/../logs/access.log', { flags: 'a' } );
+var fs = require('fs');
 
 module.exports = {
 
@@ -29,11 +28,6 @@ module.exports = {
 
     grunt: {
         _hookTimeout: 1000000
-    },
-
-    middleware: {
-
-        morgan: require('morgan')('combined', { stream: accessLogStream })
     },
 
     session: {
